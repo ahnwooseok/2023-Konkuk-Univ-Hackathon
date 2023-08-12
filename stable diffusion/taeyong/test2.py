bac3cbd1161df7eb77adf325d9e7be8ce9c424c7
jangtaeyong <jang88002@naver.com>
2023-08-12 21:02:56 +0900

    commit 08/12 21:02

--- stable diffusion/taeyong/test2.py
+++ stable diffusion/taeyong/test2.py
@@ -13,5 +13,17 @@ input = Image.open(input_path)
 # Removing the background from the given Image
 output = remove(input)
 
-# Saving the image in the given path
+
+width, height = output.size
+
+for x in range(width):
+    for y in range(height):
+        r = output.getpixel((x, y))
+        # 픽셀이 검정색(0, 0, 0)인 경우만 처리 (배경은 흰색이므로)
+        if r == (0, 0, 0, 0):
+            output.putpixel((x, y), (0, 0, 0))
+        else:
+            output.putpixel((x, y), (255, 255, 255))
+
+
 output.save(output_path)
