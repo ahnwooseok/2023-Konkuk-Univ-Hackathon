dc70b7ef927eee22f8a5744b111025ab901ad32e
ahnwooseok <peterdntjr@naver.com>
2023-08-15 00:29:36 +0900

    inject index mobile detecter

--- templete/subin/src/component/index.jsx
+++ templete/subin/src/component/index.jsx
@@ -9,28 +9,53 @@ import faceid from "../img/faceid.png"
 import upload from "../img/upload.png"
 import camera from "../img/camera.png"
 import time from '../img/time.png'
+import useMobile from "../hooks/useMobile";
 export default function Index() {
     const templates = [Template1, Template2, Template3, Template4, Template5];
     const randomIndex = Math.floor(Math.random() * templates.length);
     const RandomTemplate = templates[randomIndex];
     const [selectedGender, setSelectedGender] = useState("");
+
+    const isMobile = useMobile()
+    useEffect(()=>{
+        console.log(isMobile)
+    },[])
     return (
-      <div className='index'>
-        <h1 id="main-title">Retro Magazine Converter</h1>
-        <div id="converbtn"><img id="time"src={time}></img></div>
-        <div>
-        <div className='main-left'>
-            <div className='file'>
-                <img src={faceid} id="faceid"></img>
-                <div className='upload'><img id="upload"src={upload}></img>파일 업로드</div>
-                <div className='camera'><img id="camera"src={camera}></img>사진 촬영</div>
+        isMobile ?
+            <div className='index'>
+                <h1 id="main-title">모바일 화면입니다.</h1>
+                <div id="converbtn"><img id="time"src={time}></img></div>
+                <div>
+                    <div className='main-left'>
+                        <div className='file'>
+                            <img src={faceid} id="faceid"></img>
+                            <div className='upload'><img id="upload"src={upload}></img>파일 업로드</div>
+                            <div className='camera'><img id="camera"src={camera}></img>사진 촬영</div>
+                        </div>
+                    </div>
+                </div>
+                <div className='main-right'>
+                    <RandomTemplate />
+                </div>
             </div>
-        </div>
-        </div>
-        <div className='main-right'>
-        <RandomTemplate />
-        </div>
-       
-      </div>
+            :
+
+            <div className='index'>
+                <h1 id="main-title">Retro Magazine Converter</h1>
+                <div id="converbtn"><img id="time"src={time}></img></div>
+                <div>
+                    <div className='main-left'>
+                        <div className='file'>
+                            <img src={faceid} id="faceid"></img>
+                            <div className='upload'><img id="upload"src={upload}></img>파일 업로드</div>
+                            <div className='camera'><img id="camera"src={camera}></img>사진 촬영</div>
+                        </div>
+                    </div>
+                </div>
+                <div className='main-right'>
+                    <RandomTemplate />
+                </div>
+
+            </div>
     );
-  }
+}
