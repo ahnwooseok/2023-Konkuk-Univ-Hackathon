8f89bd61106d974419fee1513e8c9de51ca0dba3
binllionaire <bug20474@gmail.com>
2023-08-15 01:17:41 +0900

    git : update

--- templete/subin/src/component/index.jsx
+++ templete/subin/src/component/index.jsx
@@ -9,6 +9,7 @@ import faceid from "../img/faceid.png"
 import upload from "../img/upload.png"
 import camera from "../img/camera.png"
 import time from '../img/time.png'
+import Marquee from "react-fast-marquee";
 export default function Index() {
     const templates = [Template1, Template2, Template3, Template4, Template5];
     const randomIndex = Math.floor(Math.random() * templates.length);
@@ -16,19 +17,24 @@ export default function Index() {
     const [selectedGender, setSelectedGender] = useState("");
     return (
       <div className='index'>
-        <h1 id="main-title">Retro Magazine Converter</h1>
-        <div id="converbtn"><img id="time"src={time}></img></div>
         <div>
         <div className='main-left'>
+        <h1 id="main-title">레트로 잡지 표지 생성기</h1>
+        <p id="main-subtitle">설명이 들어갑니다.설명이 들어갑니다. 설명이 들어갑니다.설명이 들어갑니다.</p>
             <div className='file'>
                 <img src={faceid} id="faceid"></img>
                 <div className='upload'><img id="upload"src={upload}></img>파일 업로드</div>
                 <div className='camera'><img id="camera"src={camera}></img>사진 촬영</div>
             </div>
+            <div className='converbtn'>생성하기</div>
         </div>
         </div>
         <div className='main-right'>
-        <RandomTemplate />
+        <Marquee style={{margin:'50px'}}>
+            <Template1 />
+            <Template2 />
+            <Template3 />
+        </Marquee>
         </div>
        
       </div>
