31fadde4da3830b3d07c419dd7d185a849a68ee2
binllionaire <bug20474@gmail.com>
2023-08-16 16:35:48 +0900

    style : template css

--- templete/subin/src/component/templete1.jsx
+++ templete/subin/src/component/templete1.jsx
@@ -13,21 +13,21 @@ export default function Templete1() {
            
             <div class="header">
                 <h1 class="title">
-                    선데이서울
+                영레이디
                 </h1>
             </div>
             <div class="left">
-                <p id="t1_content1">본문내용1</p>
-                <p id="t1_content2">본문내용2</p>
-                <p id="t1_content3">본문내용3</p>
+                <p id="t1_content1">프레시지, 다이어트 <br/>도시락 3종 쿠팡 선런칭</p>
+                <p id="t1_content2">뉴진스, 아이돌그룹 브랜드평판 1위</p>
+                <p id="t1_content3">상온상압 초전도체가 뭐길래</p>
                
             </div>
             <div class="right">
-                <p id="t1_content4">본문내용4</p>
-                <p id="t1_content5">본문내용5</p>
-                <p id="t1_content6">본문내용6</p>
-                <p id="t1_content7">본문내용7</p>
-                <p id="t1_content8">본문내용8</p>
+                <p id="t1_content4">하트시그널4, '마음 <br/>통하는 기적' 일어날까</p>
+                <p id="t1_content5">넷플릭스 '19/20'<br/>(열아홉스물) 공개... </p>
+                <p id="t1_content6">한강 모래사장에<br/>선베드 깔린다…<br/>'뷰 맛집' 대변신</p>
+                <p id="t1_content7">국민 10명 중 3명 “광복절에 일본 여행, <br/>개인의 자유”</p>
+                <p id="t1_content8">동대문엽기떡볶이 '엽포터즈', 복날히든메뉴 <br/>'엽기닭볶음탕' 소개</p>
                
             </div>
             <BarcodeItemScreen/>
--- templete/subin/src/component/templete4.jsx
+++ templete/subin/src/component/templete4.jsx
@@ -3,35 +3,48 @@ import BarcodeItemScreen from './Barcode';
 import { useEffect, useState } from 'react'
 
 
-
+const textPixelChanger = str => {
+    let strLen = str.length;
+    switch (true) {
+      case 1 <= strLen && strLen < 10:
+        return "28px";
+      case 10 <= strLen && strLen < 25:
+        return "20px";
+      case 25 <= strLen && strLen < 40:
+        return "16px";
+      default:
+        return "20px";
+    }
+  };
+  
 export default function Templete4() {
-   
+    
+
     return(
 <html>
    
     <body>
         <div class="templete4">
-           
             <div class="header">
                 <h1 class="title4">
-                    QUEEN
+                    우먼센스
                 </h1>
                 
             </div>
             <div class="sec1">
-                <p id="t4_content1">본문내용1</p>
-                <p id="t4_content2">본문내용2</p>
-                <p id="t4_content3">본문내용3</p>
+                <p id="t4_content1" style={{fontSize:textPixelChanger("프레시지, 다이어트 도시락 3종 쿠팡 선런칭")}}>프레시지, 다이어트 도시락 3종 쿠팡 선런칭</p>
+                <p id="t4_content2">뉴진스, 아이돌그룹 브랜드평판 1위</p>
+                <p id="t4_content3">상온상압 초전도체가 뭐길래</p>
             </div>
             <div class="sec2">
-                <p id="t4_content4">본문내용4</p>
-                <p id="t4_content5">본문내용5</p>
-                <p id="t4_content6">본문내용6</p>
+                <p id="t4_content4">하트시그널4, '마음 통하는 기적' 일어날까</p>
+                <p id="t4_content5">넷플릭스 '19/20' (열아홉스물) 공개...</p>
+                <p id="t4_content6">한강 모래사장에 선베드 깔린다…'뷰 맛집' 대변신</p>
                
             </div>
             <div class="sec3">
-                <p id="t4_content7">본문내용7</p>
-                <p id="t4_content8">본문내용8</p>
+                <p id="t4_content7" >국민 10명 중 3명 “광복절에 일본 여행, 개인의 자유”</p>
+                <p id="t4_content8">동대문엽기떡볶이 '엽포터즈', 복날히든메뉴 '엽기닭볶음탕'</p>
                 
                
             </div>
