087d52c3379a1c94b1deea35e1eeb0af96efe003
binllionaire <bug20474@gmail.com>
2023-08-16 17:25:06 +0900

    feat : barcodenumber -> today's date

--- templete/subin/src/component/Barcode.jsx
+++ templete/subin/src/component/Barcode.jsx
@@ -1,9 +1,16 @@
 import JsBarcode from 'jsbarcode'
 import { useEffect, useState } from 'react'
-
+function getFormattedDate() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, '0');
+  return `${year}${month}${day}`;
+}
 const BarcodeItemScreen = () => {
+  
   const [imageUrl, setImageUrl] = useState("")
-  const barcodeNumber = "20230812"
+  const barcodeNumber = getFormattedDate()
   useEffect(() => {
     const canvas = document.createElement('canvas')
     JsBarcode(canvas, barcodeNumber, { margin: 8,fontSize: 15, height: 30, width:1.5, displayValue: true })
