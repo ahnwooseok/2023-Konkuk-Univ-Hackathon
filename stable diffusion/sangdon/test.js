c0d36c1d1b3451c0634e822ca333adfada51cd71
ssangdon <tngustkdehs@naver.com>
2023-08-16 17:43:22 +0900

    make_titlemaker_function

--- stable diffusion/sangdon/test.js
+++ stable diffusion/sangdon/test.js
@@ -13,3 +13,24 @@ const textPixelChanger = str => {
 };
 
 textPixelChanger("오늘의 날씨는 매우 맑음");
+
+const AgeGroupAndGenderMake = (age, genders) => {
+  let str = age;
+  let gender = "";
+  let sum = 0;
+  switch (genders) {
+    case "female":
+      gender = "여성";
+      break;
+    case "male":
+      gender = "남성";
+      break;
+  }
+  let splited = str.split("~").map(d => parseInt(d));
+  splited.map(data => (sum += data));
+  let mok = parseInt(sum / 2 / 10);
+  console.log(`${mok * 10}대 ${gender}`);
+  return `${mok * 10}대 ${gender}`;
+};
+
+AgeGroupandGenderMake("20~26", "female");
